9b71b67dbef94d1284f809627e278b932d1fa119
Gleb <fedorov22134@gmail.com>
2025-06-14 16:48:50 +0300

    edit settings for upload dir

--- src/config/core/settings.py
+++ src/config/core/settings.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
+from pathlib import Path
+
 import os
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class DataBaseSettings(BaseModel):
     """Class with database settings params"""
@@ -23,10 +27,26 @@ class RouterSettings(BaseModel):
     auth_tag: str = "Auth"
     auth_prefix: str = "/auth"
 
+    case_tag: str = "Case"
+    case_prefix: str = "/case"
+
+class FileSettings(BaseModel):
+    """Class with file settings params"""
+    UPLOAD_DIR: str = BASE_DIR / "uploads"
+    UPLOAD_IMAGE_DIR: str = BASE_DIR / f"{UPLOAD_DIR}/images"
+
 class Settings(BaseSettings):
     """Main settings class with all settings classes"""
     database_settings: DataBaseSettings = DataBaseSettings()
     token_settings: TokenSettings = TokenSettings()
     router_settings: RouterSettings = RouterSettings()
+    file_settings: FileSettings = FileSettings()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for _, v in self.file_settings.model_dump().items():
+            path = v
+            os.makedirs(path, exist_ok=True)
 
 settings = Settings()
